c9ba498cc46ced189a5aab123e829cc9aed5da6f
Andre Miras <andre.miras@gmail.com>
2020-09-29 22:10:34 +0200

    :sparkles: Publish to GitHub Pages
    After running the `yarn deploy` the dashboard would be available at:
    https://rphansen91.github.io/uniswap-dashboard/
    Note we switched to the `HashRouter` as this is more easily
    supported on GitHub pages.
    Until this is merged and deployed a demo is available via:
    https://andremiras.github.io/uniswap-dashboard/

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { Suspense, lazy } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import LinearProgress from '@material-ui/core/LinearProgress'
 
 const Home = lazy(() => import('./routes/Home'));
@@ -9,11 +9,13 @@ const Uniswap = lazy(() => import('./routes/Uniswap'));
 function App() {
   return (
     <Suspense fallback={<LinearProgress />}>
-      <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route path="/uniswap" component={Uniswap}/>
-        <Route path="/about" component={About}/>
-      </Switch>
+      <Router basename="{process.env.PUBLIC_URL}">
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route path="/uniswap" component={Uniswap}/>
+          <Route path="/about" component={About}/>
+        </Switch>
+      </Router>
     </Suspense>
   );
 }
